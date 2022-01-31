984344fde252ba4a67a376247a85c7e065b8376c
tommipoiko <tommi.poiko@gmail.com>
2022-01-31 19:31:29 +0200

    Added a studentregister application

---
+++ Java/studentregister/Student.java
@@ -0,0 +1,31 @@
+// import here
+
+public class Student {
+
+    // Private
+    private final String name;
+    private final String studentNumber;
+    
+    // Public
+    public Student(String name, String studentNumber) {
+        
+        //
+        
+        this.name = name;
+        this.studentNumber = studentNumber;
+    }
+    
+    public String getName() {
+        
+        //
+        
+        return this.name;
+    }
+    
+    public String getStudentNumber() {
+        
+        //
+        
+        return this.studentNumber;
+    }
+}
